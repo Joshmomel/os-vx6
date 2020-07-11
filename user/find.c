07528f4a06c51cb23a8f4f101875ecd0f267a94c
Josh <joshmemel@gmail.com>
2020-07-12 09:35:50 +1000

    find start

---
+++ user/find.c
@@ -0,0 +1,102 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+int match(char *path, char *name)
+{
+  char *p;
+
+  // Find first character after last slash.
+  for (p = path + strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  if (strcmp(p, name) == 0)
+    return 1;
+  else
+    return 0;
+}
+
+char *
+fmtname(char *path)
+{
+  static char buf[DIRSIZ + 1];
+  char *p;
+
+  // Find first character after last slash.
+  for (p = path + strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if (strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
+  return buf;
+}
+
+void find(char *path, char *name)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if ((fd = open(path, 0)) < 0)
+  {
+    fprintf(2, "ls: cannot open %s\n", path);
+    return;
+  }
+
+  if (fstat(fd, &st) < 0)
+  {
+    fprintf(2, "ls: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch (st.type)
+  {
+  case T_FILE:
+    if (match(name, fmtname(path)))
+    {
+      printf("%s\n", path);
+    }
+    break;
+
+  case T_DIR:
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+    {
+      printf("ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf + strlen(buf);
+    *p++ = '/';
+    while (read(fd, &de, sizeof(de)) == sizeof(de))
+    {
+      if (de.inum == 0)
+        continue;
+      if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      find(buf, name);
+    }
+    break;
+  }
+  close(fd);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 3)
+  {
+    printf("argc is %d and it is less then 2\n", argc);
+    exit();
+  }
+  find(argv[1], argv[2]);
+  exit();
+}
