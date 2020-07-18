3df0ac8d3c5cd83370cda9beca28d23fd1338019
Josh <joshmemel@gmail.com>
2020-07-18 10:03:28 +1000

    nsh

--- user/nsh.c
+++ user/nsh.c
@@ -3,8 +3,42 @@
 #include "kernel/param.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[])
+struct cmd
+{
+  int type;
+};
+
+int getcmd(char *buf, int nbuf)
 {
   fprintf(2, "@ ");
+  memset(buf, 0, nbuf);
+  gets(buf, nbuf);
+  if (buf[0] == 0) // EOF
+    return -1;
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  static char buf[100];
+
+  while (getcmd(buf, sizeof(buf)) >= 0)
+  {
+    if (buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' ')
+    {
+      // Chdir must be called by the parent, not the child.
+      printf("strlen of buf is %d\n", strlen(buf));
+      buf[strlen(buf) - 1] = 0; // chop \n
+      if (chdir(buf + 3) < 0)
+        fprintf(2, "cannot cd %s\n", buf + 3);
+      continue;
+    }
+    if (fork() == 0)
+    {
+      printf("in the child process\n");
+    }
+    wait(0);
+  }
+
   exit(0);
 }
