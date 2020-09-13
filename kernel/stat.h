57a558b0cce19ea0001e1c2b6973901930213505
Josh <joshmemel@gmail.com>
2020-09-13 11:31:11 +1000

    init symlink

--- kernel/stat.h
+++ kernel/stat.h
@@ -1,8 +1,10 @@
-#define T_DIR     1   // Directory
-#define T_FILE    2   // File
-#define T_DEVICE  3   // Device
+#define T_DIR 1     // Directory
+#define T_FILE 2    // File
+#define T_DEVICE 3  // Device
+#define T_SYMLINK 4 // Symbolic
 
-struct stat {
+struct stat
+{
   int dev;     // File system's disk device
   uint ino;    // Inode number
   short type;  // Type of file
